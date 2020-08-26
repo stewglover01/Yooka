3c58331c4500e160fd89d1ba9edb8f5e12244723
Stewart Glover <stewart.glover191@gmail.com>
2020-08-26 14:34:12 +0100

    added devise views, updated navbar with sign out and updated sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :journeys
-  # validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, presence: true
 end
