ab090f17ffc0ab245fee85c6c8e0896b4eb23f62
Stewart Glover <stewart.glover191@gmail.com>
2020-08-26 17:00:53 +0100

    updated links and included forms

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,6 @@
 class Lesson < ApplicationRecord
   has_many :journey_lessons
-  has_many :activites, dependent: :destroy
+  has_many :activities, dependent: :destroy
   has_many :videos, dependent: :destroy
   validates :name, :description, presence: true
 end
