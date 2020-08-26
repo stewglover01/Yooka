38a828e1d28965c6ecb9e45bf6791c79888171a2
Bill Bawden <wabawden@aol.com>
2020-08-26 14:52:49 +0100

    added badge functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def home
     @journeys = current_user.journeys
+    @user_badges = BadgeUser.where(user: current_user)
     @daily_mood = DailyMood.find_by(created_at: Date.today.beginning_of_day..Date.today.end_of_day, user: current_user)
     unless @daily_mood
       redirect_to '/checkin'
