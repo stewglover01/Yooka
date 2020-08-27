9f4584e7e074774d376fdffb10332d358ac58ca4
Bill Bawden <wabawden@aol.com>
2020-08-27 09:03:43 +0100

    working on streak logic

--- app/models/journey_habit.rb
+++ app/models/journey_habit.rb
@@ -1,4 +1,5 @@
 class JourneyHabit < ApplicationRecord
   belongs_to :journey
   belongs_to :habit
+  validates_uniqueness_of :habit, conditions: -> { where("DATE(created_at) = ?", Date.today) }
 end
