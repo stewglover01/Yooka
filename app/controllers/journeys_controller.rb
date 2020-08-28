7c0666ce8be439aea9341eb26f21cd1aa9ba454b
lachlan oreo <lachlan@hey.com>
2020-08-28 13:20:10 +0100

    restrict access to journey page

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -6,5 +6,30 @@ class JourneysController < ApplicationController
     @journey_lessons2 = @journey.journey_lessons
     @completed_lessons = @journey_lessons2.where(complete: true)
     @journey_progress = ((@completed_lessons.length.to_f / @journey_lessons.length)*100).to_i
+
+
+    # @last_lesson_complete = @journey_lessons.find { |journey_lesson| journey_lesson @unanswered_questions.count == 0}
+
+    # Marking as complete logic
+    @complete_journey_lessons = []
+    @journey_lessons.each do |journey_lesson|
+      @user_responses = []
+      journey_lesson.lesson.activities.each do |activity|
+          @user_responses << activity.responses.where(user: current_user)
+
+      end
+      @all_lesson_questions = [] 
+      journey_lesson.lesson.activities.each do |activity|
+        @all_lesson_questions << activity.questions
+      end
+      @unanswered_questions = @all_lesson_questions.flatten - @user_responses.flatten.map { |response| response.question }
+      if @unanswered_questions.count == 0
+        @complete_journey_lessons << journey_lesson 
+        journey_lesson.complete = true
+        journey_lesson.save!
+      end
+    end
+    @last_lesson_completed = @complete_journey_lessons.flatten.last.lesson
+    @next_lesson_to_complete = Lesson.find(@last_lesson_completed.id + 1)
   end
 end
