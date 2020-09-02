00f9b40656dffb510645ac7bfcaf484383075c8e
Liam Barlow <liamgbarlow@gmail.com>
2020-09-02 14:20:53 +0100

    added JS for NAV which works on pages without the error. Also amended Lesson page with right fonts/size based on figma. Also removed shadow from image

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -32,6 +33,8 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initUpdateNavbarOnScroll();
+
 });
 
 import "controllers"
