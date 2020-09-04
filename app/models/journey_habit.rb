2b3234e82bc814d0aeadb83257f04d6d548462f2
lachlan oreo <lachlan@hey.com>
2020-09-04 08:58:34 +0100

    bug crushiiiiing

--- app/models/journey_habit.rb
+++ app/models/journey_habit.rb
@@ -2,7 +2,7 @@ class JourneyHabit < ApplicationRecord
   belongs_to :journey
   belongs_to :habit
   has_many :questions, through: :habits
-  validates_uniqueness_of :habit, conditions: -> { where("DATE(created_at) = ?", Date.today) }
+  # validates_uniqueness_of :habit, conditions: -> { where("DATE(created_at) = ?", Date.today) }
   def self.totals_by_user(current_user)
     @students = User.where(teacher: false, school_class:current_user.school_class)
     @journeyhabits = JourneyHabit.where(@students.include? :user)
